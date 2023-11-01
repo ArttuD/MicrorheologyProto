4c1c83beec170e46e7409819cba8319e45d9ad69
Lehtonen Arttu <arttu.lehtonen@aalto.fi>
2023-11-01 10:57:57 +0200

    are we updated?

--- tools/fit.py
+++ tools/fit.py
@@ -24,7 +24,9 @@ class fit():
         self.fps = fps
 
         self.start = int(start_stamp/(1/fps))
-        self.end =int(end_stamp/(1/fps))
+        self.end = int(end_stamp/(1/fps))
+
+    
 
     def download_numpy(self, path):
 
@@ -153,16 +155,8 @@ class fit():
     def process_single(self, path_pickle, path_numpy):
 
         df = self.download_pickle(path_pickle)
+
         df_driver = self.download_numpy(path_numpy)
-
-        self.epsilon = np.mean(df["r"].values[self.end-20:self.end])
-        self.t_c = self.end_time
-
-        t_hat_creep = df["time"][self.start:self.end]-df["time"].values[self.start]
-        t_hat_rec = df["time"][self.end:]-df["time"].values[self.start]
-
-        t_pred_creep = np.arange(t_hat_creep.min(),t_hat_creep.max(), 0.001)
-        t_pred_rec = np.arange(t_hat_rec.min(),t_hat_rec.max(), 0.001)
 
         fig, ax = plt.subplots(1,2, figsize=(10,7), facecolor="white")
 
@@ -184,36 +178,45 @@ class fit():
 
         fig, ax = plt.subplots(2,2, figsize=(10,10), facecolor="white")
 
+        df["r"] = self.normalize(df["r"])
+        df["time"] = self.normalize(df["time"])
+
+        t_hat_creep = df["time"][self.start:self.end]-df["time"].values[self.start]
+        t_hat_rec = df["time"][self.end:]-df["time"].values[self.start]
+
+        t_pred_creep = np.arange(t_hat_creep.min(),t_hat_creep.max(), 0.001)
+        t_pred_rec = np.arange(t_hat_rec.min(),t_hat_rec.max(), 0.001)
+
+        self.epsilon = np.mean(df["r"].values[self.end-20:self.end])
+        self.t_c = df["time"].values[self.end]
+
+
         ax[0,0].set_title("Weibull")
-        p, p_ = curve_fit(self.Weibull_creep, t_hat_creep, df["r"][self.start:self.end], maxfev = 10000)
-        creep_label = "Creep: tau: {}".format(np.round(p[-1],5))
-        ax[0,0].plot(t_pred_creep, self.Weibull_creep(t_pred_creep,*p), label = creep_label, color = "red")
+        p, p_ = curve_fit(self.Weibull_creep, t_hat_creep, df["r"][self.start:self.end], bounds=([0,0,0,0], [np.inf,np.inf, 1, np.inf]), maxfev = 10000, method = "trf")
+        #creep_label = "Creep: tau: {}".format(np.round(p[-1],5))
+        #ax[0,0].plot(t_pred_creep, self.Weibull_creep(t_pred_creep,*p), label = creep_label, color = "red")
 
-        p, p_ = curve_fit(self.Weibull_rec, t_hat_rec, df["r"][self.end:], maxfev = 10000)
-        creep_label = "Rel: tau: {}".format(np.round(p[-1],5))
+        p_1, p__ = curve_fit(self.Weibull_rec, t_hat_rec, df["r"][self.end:], bounds=([0,0,0,0], [np.inf,np.inf, 1, np.inf]), maxfev = 10000, method = "trf")
+        creep_label = "Creep: tau: {}\nRel: tau: {}".format(np.round(p[-1],5),np.round(p[-1],5))
 
-        ax[0,0].plot(t_pred_rec, self.Weibull_rec(t_pred_rec,*p), label = creep_label, color = "orange")
+        ax[0,0].plot(np.append(t_pred_creep,t_pred_rec), np.append(self.Weibull_creep(t_pred_creep,*p),self.Weibull_rec(t_pred_rec,*p_1)), label = creep_label, color = "red")
         ax[0,0].scatter(df["time"][self.start:]-df["time"][self.start], df["r"][self.start:], color = "black", label = "data")
+
         ax[0,0].set_xlabel("time (s)")
         ax[0,0].set_ylabel(r" $\epsilon$ ($\mu$m)")
         ax[0,0].legend(loc = "upper left",fontsize = 6)
 
-        ax[0,1].set_title("Maxwell")
-        p, p_ = curve_fit(self.maxwell_creep, t_hat_creep, df["r"][self.start:self.end], maxfev = 10000)
-        creep_label = "Creep: tau: {}".format(np.round(p[1]/p[0],5))
-        ax[0,1].plot(t_pred_creep, self.maxwell_creep(t_pred_creep,*p), label = creep_label, color = "red")
-
-        p, p_ = curve_fit(self.maxwell_rec, t_hat_rec, df["r"][self.end:], maxfev = 10000)
-        creep_label = "Rel: tau: {}".format(np.round(p[1]/p[0],5))
-        ax[0,1].plot(t_pred_rec, self.maxwell_rec(t_pred_rec,*p), label = creep_label, color = "orange")
+        ax[0,1].set_title("Maxwell model")
+        p, pcov = curve_fit(self.combine_maxwell, df["time"][self.start:] - df["time"][self.start], df["r"][self.start:], bounds=([0,0], [np.inf,np.inf]), maxfev = 1000000, method = "trf")
+        maxwell_label = "maxwell: \ntau: {}".format(np.round(p[-1]/p[1],5))
+        ax[0,1].plot(np.append(t_pred_creep,t_pred_rec), np.append(self.maxwell_creep(t_pred_creep,*p), self.maxwell_rec(t_pred_rec,*p) ), label = maxwell_label,color = "red")
         ax[0,1].scatter(df["time"][self.start:]-df["time"][self.start], df["r"][self.start:], color = "black", label = "data")
+        ax[0,1].legend(loc = "upper left",fontsize = 6)
         ax[0,1].set_xlabel("time (s)")
         ax[0,1].set_ylabel(r" $\epsilon$ ($\mu$m)")
-        ax[0,1].legend(loc = "upper left",fontsize = 6)
 
         ax[1,0].set_title("Burger model")
-        p, pcov = curve_fit(self.combine_burger, df["time"][self.start:] - df["time"][self.start], df["r"][self.start:], maxfev = 100000)
-
+        p, pcov = curve_fit(self.combine_burger, df["time"][self.start:] - df["time"][self.start], df["r"][self.start:], bounds=([0,0,0,0], [np.inf,np.inf,np.inf,np.inf]), maxfev = 1000000, method = "trf")
         burger_label = "Burger: \ntau: {}".format(np.round(p[-1]/p[1],5))
         ax[1,0].plot(np.append(t_pred_creep,t_pred_rec), np.append(self.burger_creep(t_pred_creep,*p), self.burger_rec(t_pred_rec,*p) ), label = burger_label,color = "red")
         ax[1,0].scatter(df["time"][self.start:]-df["time"][self.start], df["r"][self.start:], color = "black", label = "data")
@@ -222,7 +225,7 @@ class fit():
         ax[1,0].set_ylabel(r" $\epsilon$ ($\mu$m)")
 
         ax[1,1].set_title("Kelvin-Voigt")
-        p, pcov = curve_fit(self.combine_kelvin, df["time"][self.start:] - df["time"][self.start], df["r"][self.start:], maxfev = 100000)
+        p, pcov = curve_fit(self.combine_kelvin, df["time"][self.start:] - df["time"][self.start], df["r"][self.start:],bounds=([0,0], [np.inf,np.inf]), maxfev = 1000000, method = "trf")
 
         burger_label = "kelvin: \ntau: {}".format(np.round(p[-1]/p[1],5))
         ax[1,1].plot(np.append(t_pred_creep,t_pred_rec), np.append(self.kelvin_creep(t_pred_creep,*p), self.kelvin_rec(t_pred_rec,*p) ), label = burger_label,color = "red")
@@ -255,9 +258,23 @@ class fit():
         result2 = self.kelvin_rec(extract2, E_1, n_1)
 
         return np.append(result1, result2)
+    
+    def combine_maxwell(self, x_tot, E_1, n_1):
 
+        # single data reference passed in, extract separate data
+        extract1 = x_tot[:(self.end-self.start)] # first data
+        extract2 = x_tot[(self.end-self.start):] # second data
+
+        result1 = self.maxwell_creep(extract1, E_1, n_1)
+        result2 = self.maxwell_rec(extract2, E_1, n_1)
+
+        return np.append(result1, result2)
+
+    def normalize(self, data):
+        return (data - np.min(data)) / (np.max(data) - np.min(data))
+    
     def maxwell_rec(self, x, E_1, n_1 ):
-        return  self.epsilon*np.exp(-(x-self.t_c)*E_1/n_1)
+        return  self.epsilon-1/E_1+0*x*n_1
     
     def maxwell_creep(self, x, E_1, n_1):
         return 1/E_1 + 1/n_1*x
@@ -272,7 +289,7 @@ class fit():
         return 1/E_1*(1-np.exp(-t*E_1/n_1))
 
     def kelvin_rec(self, t, E_1, n_1):
-        return self.epsilon*(np.exp(-(t-self.t_c)*E_1/n_1))
+        return 1/E_1*np.exp(-E_1/n_1*t)*(np.exp(-(E_1/n_1)*self.t_c)-1)
 
     def Weibull_creep(self,x, e_i, e_c, beta, tau):
         return e_i + e_c*(1-np.exp(-(x/tau)**beta))
